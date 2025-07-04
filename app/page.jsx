85226051ec17b98034feca923380eae07b5b9ab7
Tochukwu Ozo-Osiedo <tochukwuosiedo001@gmail.com>
2025-07-04 13:55:35 +0100

    nav completed

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,48 @@
+import Link from "next/link";
+import { MdOutlineArrowOutward } from "react-icons/md";
 
 export default function Home() {
   return (
-    <main>
-      Hello World
+    <main className="min-h-dvh bg-[url('/bg.jpg')] bg-no-repeat bg-center bg-cover">
+      <section className="min-h-dvh bg-black/60 flex items-center justify-center">
+        <div className="space-y-10 text-white md:px-20 p-4 lg:w-3/4">
+          <h1 className="md:text-5xl text-3xl text-center font-black">
+            Home of all Recipes. Diversity in Delicacies.
+          </h1>
+          <p className="md:text-2xl text-lg text-center">
+            Here at 
+            <span className="md:text-2xl text-blue-600 font-bold"> KookBook</span>,
+            we specialize in the collection of recipes, instructions, and
+            information about the preparation and serving of foods. It includes
+            a variety of appetizers, salads, main courses, side dishes and
+            deserts
+          </p>
+          <div className="flex justify-center gap-4 md:gap-10 max-md:flex-col">
+            <Link
+              href={"/recipes"}
+              className="bg-blue-600 text-white px-6 py-2 rounded-full font-semibold flex items-center justify-center gap-2 text-base md:text-xl hover:bg-blue-700 transition-all"
+            >
+              Explore Recipes
+              <MdOutlineArrowOutward />
+            </Link>
+            <Link
+              href={"/about"}
+              className="bg-blue-600 text-white px-6 py-2 rounded-full font-semibold flex items-center justify-center gap-2 text-base md:text-xl hover:bg-blue-700 transition-all"
+            >
+              Learn More
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <section className="bg-gray-100 min-h-[70vh] p-10">
+        <h1>Why Choose Us</h1>
+        <div className="lg:px-5 p-3 grid lg:grid-cols-3 md:grid-cols-2">
+          <div>
+            
+          </div>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -25,7 +25,7 @@ const Nav = () => {
 
   return (
     <nav className="flex items-center justify-between shadow-md py-3 px-6 relative">
-      <Link href={"/"} className="flex items-center gap-1">
+      <Link href={"/"} className="flex items-center gap-1 z-50">
         <Image
           src={"/logo.png"}
           alt="logo"
@@ -38,7 +38,7 @@ const Nav = () => {
         </p>
       </Link>
 
-      <div className="flex items-center gap-8 max-lg:hidden">
+      <div className="flex items-center gap-8 ml-auto max-lg:hidden">
         {navItems.map((item, i) => (
           <Link
             key={i}
@@ -49,29 +49,31 @@ const Nav = () => {
           </Link>
         ))}
 
+      </div>
         <Link
           href={"#"}
-          className="flex items-center gap-1 text-lg border px-3 py-1 hover:text-blue-600 hover:border-blue-600 transition-colors duration-300"
+          className="flex items-center gap-1 text-lg lg:border px-3 py-1 hover:text-blue-600 hover:border-blue-600 transition-colors duration-300 ml-8 max-lg:ml-auto z-50"
         >
           <FiUser />
-          <p>Sign In</p>
+          <p className="max-lg:hidden">Sign In</p>
         </Link>
-      </div>
 
       {/* mobile and tab view */}
-      <div className="h-dvh w-full overflow-hidden lg:hidden absolute top-0 right-0 bg-white flex flex-col items-center justify-center gap-20">
-        {navItems.map((item, i) => (
-          <Link
-            key={i}
-            href={item.url}
-            className="text-lg hover:text-blue-600 transition-colors duration-300"
-          >
-            {item.label}
-          </Link>
-        ))}
-      </div>
+      {navOpen ? (
+        <div className="h-dvh w-full overflow-hidden lg:hidden absolute top-0 right-0 bg-white flex flex-col items-center justify-center gap-20">
+          {navItems.map((item, i) => (
+            <Link
+              key={i}
+              href={item.url}
+              className="text-lg hover:text-blue-600 transition-colors duration-300"
+            >
+              {item.label}
+            </Link>
+          ))}
+        </div>
+      ) : null}
 
-      <div className="lg:hidden">
+      <div className="lg:hidden z-50 mt-1">
         <button onClick={handleOpen} className="text-2xl">
           {navOpen ? <IoMdClose /> : <IoMdMenu />}
         </button>
