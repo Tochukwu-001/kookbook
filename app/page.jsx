90adadbd4f858cb3c2862362a0d8587caf496235
Marigold <uchemarigold@gmail.com>
2025-07-16 14:19:19 +0100

    auth

--- app/page.jsx
+++ app/page.jsx
@@ -2,16 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 import { MdOutlineArrowOutward } from "react-icons/md";
 
-<<<<<<< HEAD
-
-export default function Home() {
-  const chooseUs = [
-     {
-=======
 export default function Home() {
   const chooseUs = [
     {
->>>>>>> 9abe13dc891d6c42247cefbd4ddde745048d9846
       image: "/c1.jpg",
       title: "Expert Culinary Culture",
       desc: "Choose us for our expertise, unique perspective, commitment to quality and continous improvement. We showcase a strong passion for food by staying current with culinary trends and techniques.",
@@ -27,37 +20,6 @@ export default function Home() {
       desc: "We inspire our clients to share their various traditional and native recipes and perspective on foods and cooking adding depth and richness to your kitchen desires.",
     },
   ];
-<<<<<<< HEAD
-
-  return (
-    <main className="min-h-dvh bg-[url('/bg.jpg')] bg-no-repeat bg-center bg-cover">
-      <section className="min-h-dvh bg-black/60 flex items-center justify-center ">
-        <div className="space-y-10 text-white md:px-20 p-4 lg:w-3/4">
-          <h1 className="md:text-5xl text-3xl text-center font-black">Home of all Recipes. Diversity in Delicacies.
-
-          </h1>
-          <p className="md:text-2xl text-lg text-center">
-            Here at <span className="md:text-2xl text-blue-600 font-bold">Kookbook</span>, we specialize in the collection of recipes, instructions,
-            and information about the preparation of
-            serving of foods. It includes a variety of appetizers, salads, main courses,
-            desserts, and beverages.
-          </p>
-          <div className="flex justify-center gap-4 md:gap-10 max-md:flex-col">
-            <Link href="/recipes"
-              className="bg-blue-600 text-white px-6 py-2  rounded-full font-semibold flex items-center  justify-center 
-             gap-2 text-base md:text-xl hover:bg-blue-700 transition all">Explore Recipes
-              <MdOutlineArrowOutward />
-
-            </Link>
-
-            <Link href="/about"
-              className="bg-blue-600 text-white px-6 py-2  rounded-full font-semibold flex items-center  justify-center 
-             gap-2 text-base md:text-xl hover:bg-blue-700 transition all">Learn More </Link>
-          </div>
-        </div>
-
-
-=======
   return (
     <main className="min-h-dvh bg-[url('/bg.jpg')] bg-no-repeat bg-center bg-cover">
       <section className="min-h-dvh bg-black/60 flex items-center justify-center">
@@ -92,7 +54,6 @@ export default function Home() {
             </Link>
           </div>
         </div>
->>>>>>> 9abe13dc891d6c42247cefbd4ddde745048d9846
       </section>
 
       <section className="bg-gray-100 min-h-[70vh] p-3 md:p-10">
@@ -107,12 +68,6 @@ export default function Home() {
           ))}
         </div>
       </section>
-<<<<<<< HEAD
-      
     </main>
-
-=======
-    </main>
->>>>>>> 9abe13dc891d6c42247cefbd4ddde745048d9846
   );
 }
