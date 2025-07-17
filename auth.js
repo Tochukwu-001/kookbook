67b54a1e6a0d7d303729aa86d22b3e86b2b28432
Paul salifu <salifupaul48@gmail.com>
2025-07-17 14:00:16 +0100

    db

--- auth.js
+++ auth.js
@@ -1,5 +1,25 @@
 import NextAuth from "next-auth"
- 
+ import GoogleProvider from "next-auth/providers/google";
+ import GitHubProvider from "next-auth/providers/github";
+ import { FirestoreAdapter } from "@auth/firebase-adapter"
+ import { cert } from "firebase-admin/app"
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    GoogleProvider({
+        clientId:process.env.AUTH_GOOGLE_ID,
+        clientSecret:process.env.AUTH_GOOGLE_SECRET
+    }),
+      GitHubProvider({
+    clientId: process.env.GITHUB_ID,
+    clientSecret: process.env.GITHUB_SECRET
+  })
+  ],
+   adapter: FirestoreAdapter({
+     credential: cert({
+      projectId: process.env.AUTH_FIREBASE_PROJECT_ID,
+      clientEmail: process.env.AUTH_FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.AUTH_FIREBASE_PRIVATE_KEY,
+    }),
+   })
 })
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,10 +5,26 @@ import React, { useState } from "react";
 import { FiUser } from "react-icons/fi";
 import { IoMdMenu } from "react-icons/io";
 import { IoMdClose } from "react-icons/io";
+import {useSession} from "next-auth/react";
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { signOut } from "next-auth/react"
 
 const Nav = () => {
   const [navOpen, setNavOpen] = useState(false);
+  const {data:session, status}=useSession();
 
+  console.log(session,status);
+
+   const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  
   // handler function for nav open
   const handleOpen = () => {
     setNavOpen(!navOpen);
@@ -22,6 +38,7 @@ const Nav = () => {
     { url: "/faqs", label: "FAQs" },
     { url: "/recipes", label: "Recipes" },
   ];
+
 
   return (
     <nav className="flex items-center justify-between shadow-md py-3 px-6 relative">
@@ -50,6 +67,7 @@ const Nav = () => {
         ))}
 
       </div>
+      {!session?.user?(
         <Link
           href={"/auth/signin"}
           className="flex items-center gap-1 text-lg lg:border px-3 py-1 hover:text-blue-600 hover:border-blue-600 transition-colors duration-300 ml-8 max-lg:ml-auto z-50"
@@ -58,6 +76,47 @@ const Nav = () => {
           <p className="max-lg:hidden">Sign In</p>
         </Link>
 
+      ): (
+        
+<div className="max-max:lg-auto z-50">
+      <button
+        id="demo-positioned-button"
+        aria-controls={open ? 'demo-positioned-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+      <img src={session?.user?.image} alt={session?.user?.name.slice(0,1).toUpperCase()} 
+        className="w-10 h-10 rounded-full ml-8 max-lg:ml-auto z-50"/>
+      </button>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+      >
+        <MenuItem onClick={handleClose}>
+          <Link href={"/profile"}>My Profile</Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link href={"/add-recipe"}>Add Recipe</Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+             <button onClick={() => signOut()}>Sign Out</button>
+        </MenuItem>
+      </Menu>
+    </div>
+      )}
+        
       {/* mobile and tab view */}
       {navOpen ? (
         <div className="h-dvh w-full overflow-hidden lg:hidden absolute top-0 right-0 bg-white flex flex-col items-center justify-center gap-20">
