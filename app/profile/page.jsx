bee42e733d5c67d5e551776762e5f97a45215142
Tochukwu Ozo-Osiedo <tochukwuosiedo001@gmail.com>
2025-07-24 13:57:04 +0100

    new updates

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,14 +1,35 @@
 "use server";
-import { auth } from '@/auth';
-import React from 'react'
+import { auth } from "@/auth";
+import React from "react";
 
 const page = async () => {
-  const session = await auth()
+  const session = await auth();
+  const name = session?.user?.name
+  let x = name.split(" ")
+  console.log(x[0]);
+  let username = x [0]
+  
   return (
-    <main className='min-h-dvh'>
-      my profile
+    <main className="min-h-dvh">
+      <section className="">
+        <div className="h-[30vh] bg-[url('/wavy.png')] bg-no-repeat bg-center bg-cover relative">
+          <img
+            src={session?.user?.image}
+            alt={session?.user?.name.slice(0, 1).toUpperCase()}
+            className="w-40 h-40 rounded-full absolute -bottom-[50%] left-[45%]"
+          />
+        </div>
+        <div className="mt-30">
+          <h1 className="text-center text-3xl text-gray-800 font-bold uppercase">
+            {session?.user?.name}
+          </h1>
+          <p>
+            @{username}
+          </p>
+        </div>
+      </section>
     </main>
-  )
-}
+  );
+};
 
-export default page
+export default page;
